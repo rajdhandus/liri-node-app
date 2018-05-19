74474521ccf89a9c48546be91facd1a4ab190e05
Rajkumar Purushothaman <rajkumar.purushothaman@lowes.com>
2018-05-19 19:33:32 -0400

    All functionalities completed!

--- liri.js
+++ liri.js
@@ -10,91 +10,107 @@ var stars =
 var spotify_client = new Spotify(keys.spotify);
 var twitter_client = new Twitter(keys.twitter);
 
-let param1 = process.argv
+let action = process.argv
   .slice(2, 3)
   .join("")
   .trim();
-let param2 = process.argv.slice(3).join(" ");
+let remainParams = process.argv.slice(3).join(" ");
 
-if (param1 === "my-tweets") {
-  let screenName = "RajPurushoth";
-  if (param2) {
-    screenName = param2;
-  }
-  twitter_client.get(
-    "statuses/user_timeline",
-    { screen_name: screenName, count: 20 },
-    function(error, tweets, response) {
-      if (tweets.length > 0) {
-        for (let i = 0; i < tweets.length; i++) {
-          logWriter(
-            stars +
-              " Tweet #" +
-              (i + 1) +
-              " " +
+mainFunc(action, remainParams);
+
+function mainFunc(param1, param2) {
+  if (param1 === "my-tweets") {
+    let screenName = "RajPurushoth";
+    if (param2) {
+      screenName = param2;
+    }
+    twitter_client.get(
+      "statuses/user_timeline",
+      { screen_name: screenName, count: 20 },
+      function(error, tweets, response) {
+        if (tweets.length > 0) {
+          for (let i = 0; i < tweets.length; i++) {
+            logWriter(
               stars +
-              "\n" +
-              tweets[i].text +
-              "\n" +
-              tweets[i].created_at
-          );
+                " Tweet #" +
+                (i + 1) +
+                " " +
+                stars +
+                "\n" +
+                tweets[i].text +
+                "\n" +
+                tweets[i].created_at
+            );
+          }
+        } else {
+          logWriter("No tweets for @" + screenName);
+        }
+      }
+    );
+  } else if (param1 === "spotify-this-song" && param2) {
+    logWriter(stars);
+    spotify_client.search({ type: "track", query: param2 }, function(
+      err,
+      data
+    ) {
+      if (err) {
+        logWriter("Error occurred: " + err);
+        return;
+      } else if (data.tracks.items.length > 0) {
+        logWriter("Artist(s) : " + data.tracks.items[0].album.artists[0].name);
+        logWriter("Song Name : " + data.tracks.items[0].name);
+        logWriter("Album Name : " + data.tracks.items[0].album.name);
+        if (data.tracks.items[0].preview_url === "null") {
+          logWriter("Preview Link : Not Available in Response");
+        } else {
+          logWriter("Preview Link : " + data.tracks.items[0].preview_url);
         }
       } else {
-        logWriter("No tweets for @" + screenName);
+        logWriter(param2 + " was not found in spotify ");
       }
-    }
-  );
-} else if (param1 === "spotify-this-song" && param2) {
-  logWriter(stars);
-  spotify_client.search({ type: "track", query: param2 }, function(err, data) {
-    if (err) {
-      logWriter("Error occurred: " + err);
-      return;
-    } else if (data.tracks.items.length > 0) {
-      logWriter("Artist(s) : " + data.tracks.items[0].album.artists[0].name);
-      logWriter("Song Name : " + data.tracks.items[0].name);
-      logWriter("Album Name : " + data.tracks.items[0].album.name);
-      if (data.tracks.items[0].preview_url === "null") {
-        logWriter("Preview Link : Not Available in Response");
-      } else {
-        logWriter("Preview Link : " + data.tracks.items[0].preview_url);
-      }
-    } else {
-      logWriter(param2 + " was not found in spotify ");
-    }
+      logWriter(stars);
+    });
+  } else if (param1 === "movie-this" && param2) {
+    var URL = "http://www.omdbapi.com/?apikey=trilogy&t=" + param2;
     logWriter(stars);
-  });
-} else if (param1 === "movie-this" && param2) {
-  var URL = "http://www.omdbapi.com/?apikey=trilogy&t=" + param2;
-  logWriter(stars);
-  request(URL, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      let jsonData = JSON.parse(body);
-      if (jsonData.Response === "True") {
-        logWriter("Title : " + jsonData.Title);
-        logWriter("Year : " + jsonData.Year);
-        logWriter("IMDB Rating : " + jsonData.Ratings[0].Value);
-        logWriter("Rotten Tomato Ratig : " + jsonData.Ratings[1].Value);
-        logWriter("Country : " + jsonData.Country);
-        logWriter("Language : " + jsonData.Language);
-        logWriter("Plot : " + jsonData.Plot);
-        logWriter("Actors : " + jsonData.Actors);
+    request(URL, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        let jsonData = JSON.parse(body);
+        if (jsonData.Response === "True") {
+          logWriter("Title : " + jsonData.Title);
+          logWriter("Year : " + jsonData.Year);
+          logWriter("IMDB Rating : " + jsonData.Ratings[0].Value);
+          logWriter("Rotten Tomato Ratig : " + jsonData.Ratings[1].Value);
+          logWriter("Country : " + jsonData.Country);
+          logWriter("Language : " + jsonData.Language);
+          logWriter("Plot : " + jsonData.Plot);
+          logWriter("Actors : " + jsonData.Actors);
+        } else {
+          logWriter(param2 + " was not found in OMDB");
+        }
       } else {
-        logWriter(param2 + " was not found in OMDB");
+        logWriter("An Error Happened!!" + error);
       }
-    } else {
-      logWriter("An Error Happened!!" + error);
-    }
+      logWriter(stars);
+    });
+  } else if (param1 === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function(error, response) {
+      if (error) {
+        throw error;
+      }
+
+      let params = response.split(",");
+      mainFunc(params[0], params[1]);
+    });
+  } else {
     logWriter(stars);
-  });
-} else {
-  logWriter(stars);
-  logWriter("Invalid Parameters");
-  logWriter(stars);
-  logWriter("Valid Parameters are ");
-  logWriter("   => my-tweets <twitter_handle>");
-  logWriter("   => spotify-this-song <song name>");
-  logWriter("   => movie-this <movie name>");
+    logWriter("Invalid Parameters");
+    logWriter(stars);
+    logWriter("Valid Parameters are ");
+    logWriter("   => my-tweets <twitter_handle>");
+    logWriter("   => spotify-this-song <song name>");
+    logWriter("   => movie-this <movie name>");
+  }
 }
 
 function logWriter(data, target) {
