e3901ecd29fbb0e2d73b8faa79b61602b49458de
Rajkumar Purushothaman <rajkumar.purushothaman@lowes.com>
2018-05-19 18:29:27 -0400

    Additional error handling logic

--- liri.js
+++ liri.js
@@ -2,40 +2,55 @@ require("dotenv").config();
 var Twitter = require("twitter");
 var Spotify = require("node-spotify-api");
 var request = require("request");
+var fs = require("fs");
 var keys = require("./keys");
 
 var spotify_client = new Spotify(keys.spotify);
 var twitter_client = new Twitter(keys.twitter);
 
-let param1 = process.argv[2];
-let param2 = process.argv[3];
+let param1 = process.argv
+  .slice(2, 3)
+  .join("")
+  .trim();
+let param2 = process.argv.slice(3).join(" ");
 
 if (param1 === "my-tweets") {
+  let screenName = "RajPurushoth";
+  if (param2) {
+    screenName = param2;
+  }
   twitter_client.get(
     "statuses/user_timeline",
-    { screen_name: "RajPurushoth", count: 20 },
+    { screen_name: screenName, count: 20 },
     function(error, tweets, response) {
-      console.log(tweets.length);
-      for (let i = 0; i < tweets.length; i++) {
-        console.log(
-          "******************* Tweet # " + (i + 1) + " *******************"
-        );
-        console.log(tweets[i].text);
-        console.log(tweets[i].created_at);
+      if (tweets.length > 0) {
+        for (let i = 0; i < tweets.length; i++) {
+          logWriter(
+            "**************************************************************************** Tweet #" +
+              (i + 1) +
+              " ****************************************************************************" +
+              "\n" +
+              tweets[i].text +
+              "\n" +
+              tweets[i].created_at
+          );
+        }
+      } else {
+        logWriter("No tweets for @" + screenName);
       }
-      // console.log("Tweet");
     }
   );
 } else if (param1 === "spotify-this-song" && param2) {
   spotify_client.search({ type: "track", query: param2 }, function(err, data) {
     if (err) {
-      return console.log("Error occurred: " + err);
+      logWriter("Error occurred: " + err);
+      return;
+    } else if (data.tracks.items.length > 0) {
+      logWriter("Artist(s) : " + data.tracks.items[0].album.artists[0].name);
+      logWriter("Song Name : " + data.tracks.items[0].name);
+      logWriter("Album Name : " + data.tracks.items[0].album.name);
+      logWriter("Preview Link : " + data.tracks.items[0].preview_url);
     }
-    // console.log(JSON.stringify(data));
-    console.log("Artist(s) : " + data.tracks.items[0].album.artists[0].name);
-    console.log("Song Name : " + data.tracks.items[0].name);
-    console.log("Album Name : " + data.tracks.items[0].album.name);
-    console.log("Preview Link : " + data.tracks.items[0].preview_url);
   });
 } else if (param1 === "movie-this" && param2) {
   var URL = "http://www.omdbapi.com/?apikey=trilogy&t=" + param2;
@@ -43,16 +58,37 @@ if (param1 === "my-tweets") {
   request(URL, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       let jsonData = JSON.parse(body);
-      console.log("Title : " + jsonData.Title);
-      console.log("Year : " + jsonData.Year);
-      console.log("IMDB Rating : " + jsonData.Ratings[0].Value);
-      console.log("Rotten Tomato Ratig : " + jsonData.Ratings[1].Value);
-      console.log("Country : " + jsonData.Country);
-      console.log("Language : " + jsonData.Language);
-      console.log("Plot : " + jsonData.Plot);
-      console.log("Actors : " + jsonData.Actors);
+      logWriter("Title : " + jsonData.Title);
+      logWriter("Year : " + jsonData.Year);
+      logWriter("IMDB Rating : " + jsonData.Ratings[0].Value);
+      logWriter("Rotten Tomato Ratig : " + jsonData.Ratings[1].Value);
+      logWriter("Country : " + jsonData.Country);
+      logWriter("Language : " + jsonData.Language);
+      logWriter("Plot : " + jsonData.Plot);
+      logWriter("Actors : " + jsonData.Actors);
     } else {
-      console.log("An Error Happened!!" + error);
+      logWriter("An Error Happened!!" + error);
     }
+  });
+} else {
+  logWriter("Invalid Parameters");
+  logWriter("Valid Parameters are ");
+  logWriter("my-tweets or spotify-this-song or movie-this");
+}
+
+function logWriter(data, target) {
+  if (target === 0) {
+    console.log(data);
+  } else if (target === 1) {
+    fileWriter(data, "./log.txt");
+  } else {
+    console.log(data);
+    fileWriter(data, "./log.txt");
+  }
+}
+
+function fileWriter(data, fileName) {
+  fs.appendFile(fileName, data + "\n", function(error) {
+    if (error) throw error;
   });
 }
